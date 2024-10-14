02743d61223af41562e89c146d40b6cb7703c796
Arif B <arif.dev@pm.me>
2024-10-14 14:24:20 +0530

    Refactor settings to reflect new config file format

--- settings.py
+++ settings.py
@@ -1,50 +1,24 @@
 import os
 import yaml
+from util import resolvepath, error
+import sys
 
-class Data: 
-    config = {}
-    target: str
-    tag: str
-    arches: list
-    pkg_import = {}
-    files = {}
+class Settings:
+    instance = dict()
+    pkg_import = dict()
+    files = dict()
+
     def __init__(self, configfile) -> None:
 
-        def resolvepath(path):
-            """Resolves variables like ${userHome} and ${cwd} in a given path."""
-            if path is None:
-                return None
-            
-            variables = {
-                "${userHome}": os.path.expanduser("~"),
-                "${cwd}": os.getcwd()
-            }
-
-            while "${" in path:
-                start_idx = path.find("${")
-                end_idx = path.find("}", start_idx)
-                variable = path[start_idx : end_idx + 1]
-                if variable in variables:
-                    path = path.replace(variable, variables[variable])
-                else:
-                    raise ValueError(f"Unknown variable: {variable}")
-
-            return path
-        
         with open(configfile, "r") as f:
             parameters = yaml.safe_load(f)
 
-        self.config['upstream'] = resolvepath(parameters['configfile']['upstream'])
-        self.config['downstream'] = resolvepath(parameters['configfile']['downstream'])
+        self.instance['upstream'] = parameters['instance']['upstream']
+        self.instance['downstream'] = parameters['instance']['downstream']
 
-        self.target = parameters['build_target']['target']
-        self.tag = parameters['build_target']['dest_tag']
-
-        self.arches = parameters['arches']
-
+        self.pkg_import['attempt'] = parameters['pkg_import']['attempt']
         self.pkg_import['topurl'] = parameters['pkg_import']['topurl']
         self.pkg_import['dir'] = parameters['pkg_import']['dir']
-
 
         self.files['build'] = resolvepath(parameters['files']['build'])
         self.files['ignore'] = resolvepath(parameters['files']['ignore'])
@@ -53,5 +27,10 @@ class Data:
 
 
 
-data = None
-
+try:
+    settings = Settings("config.yml")
+except FileNotFoundError:
+    try:
+        settings = Settings(sys.argv[2])
+    except IndexError:
+        error("Yaml config file required")
