7141b7c4e5799a80e9ecb22653b97e24678c04f0
Arif B <arif.dev@pm.me>
2024-10-15 08:24:51 +0530

    Refactor code to use new instance variable from session object and fetch tag info

--- rebuild.py
+++ rebuild.py
@@ -1,8 +1,9 @@
-from tasks import watch_task
+from tasks import watch_task, TaskState
 import logging
 from util import error
 import koji
 from enum import IntEnum
+import os
 
 
 class BuildState(IntEnum):
@@ -13,34 +14,37 @@ class BuildState(IntEnum):
     CANCELLED = 4
 
 
-async def rebuildPackage(upstream, downstream, pkg, target, tag = None): 
+async def rebuildPackage(upstream, downstream, pkg) -> BuildState:
     logger = logging.getLogger(__name__)
 
-    try : 
-        #FIXME : target:str won't be carrying this data
-        dest_tag = target['dest_tag_name']
-    except IndexError:
-        error("Target does not specify a destination tag")
+    result = BuildState.FAILED
+    upst_tag = None
 
-    if tag is None:
-        tag = dest_tag
-    
+    try:
+        upst_target = upstream.instance['build_target']['target']
+        upst_tag = (upstream.getBuildTarget(upst_target)).get('dest_tag_name')
+    except (koji.GenericError, KeyError) as e:
+        error(e)
+
+    tag = downstream.instance['build_target']['dest_tag']
+
     if not downstream.checkTagPackage(tag = tag, pkg = pkg):
-        logger.warning(f"No package : {pkg} in {tag} present in downstream. Adding package to {tag}")
+        logger.warning(f"No package : {pkg} associated with {tag}. Adding package to {tag}")
         downstream.packageListAdd(tag = tag, pkg = pkg)
-        
+
     if downstream.getSessionInfo() is None:
-        if downstream.login() == False:
+        if downstream.auth_login() == False:
             error("You need to be logged in to build packages!")
-    
+
     # Check if rpms for given package were built in upstream
-    builds = upstream.getLatestRPMS(dest_tag, pkg)
+    builds = upstream.getLatestRPMS(upst_tag, pkg)
+
     if not any(builds):
         logger.critical("No rpms built for package : %s in upstream" % pkg)
-        return None
-    
+        return BuildState.FAILED
+
     # Check if package with same NVR already exists in downstream
-    # TODO : Check NVR without src rpm
+    nvr_up = nvr_down = None
     b1 = builds[0]
     if b1['arch'] == 'src':
         nvr_up = "-".join([b1['name'], b1['version'], b1['release']])
@@ -56,30 +60,35 @@ async def rebuildPackage(upstream, downstream, pkg, target, tag = None):
                 nvr_down = "-".join([b2['name'], b2['version'], b2['release']])
         else:
             nvr_down = None
-        
+
     if nvr_up == nvr_down:
         logger.info("Package %s is already built and tagged under %s" % (pkg, tag))
         return BuildState.COMPLETE
 
-    # Check if package is noarch
-    if upstream.isNoArch(dest_tag, pkg):
-        # download package rpms from upstream
-        pkgpath = await upstream.downloadRPMs(dest_tag, pkg)
-        if pkgpath is not None:
-            # import package rpms to downstream and tag the package under 'tag'
-            downstream.importPackage(pkgpath, dest_tag, pkg)
-    
-    else:
-        scmurl = upstream.getSCM_URL(dest_tag, pkg)
-        if scmurl is not None:
-            task_id = downstream.build(src = scmurl, target = target)
-            try:
-                build_time = int(upstream.getAverageBuildDuration(package = pkg))
-                if build_time > 1800:
-                    poll = 3600
-            except ValueError:
-                poll = 1800
-            
-            result = await watch_task(downstream, task_id, poll_interval=poll)
+    if os.getenv('import_attempt') == 'True':
+        # Check if package is noarch
+        if upstream.isNoArch(upst_tag, pkg):
+            # download package rpms from upstream
+            pkgpath = await upstream.downloadRPMs(upst_tag, pkg)
+            if pkgpath is not None:
+                # import package rpms to downstream and tag the package under 'tag'
+                downstream.importPackage(os.getenv('import_topurl'), os.getenv('import_dir'), pkgpath, tag, pkg)
 
-        return result
+            result = BuildState.COMPLETE
+
+    else:
+        scmurl = upstream.getSCM_URL(upst_tag, pkg)
+        if scmurl is not None:
+            task_id = downstream.build(src = scmurl, target = downstream.target)
+            res = await watch_task(downstream, task_id)
+
+            if res == TaskState.CLOSED:
+                result = BuildState.COMPLETE
+            elif res == TaskState.CANCELLED:
+                result = BuildState.CANCELLED
+            elif res == TaskState.FAILED:
+                result = BuildState.FAILED
+
+    return result
+        
+    
