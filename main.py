4730d51c454a95d7df25aa315e41fb5be4794a68
Arif B <arif.dev@pm.me>
2024-10-17 18:03:06 +0530

    Separate prep from main rebuild logic using task_dispatcher

--- main.py
+++ main.py
@@ -1,70 +1,115 @@
-from kojisession import KojiSession
-import os 
+import os
 import sys
 import asyncio
 import logging
-from util import (error, whoami, warn, bootstrap)
-import settings
+import typing
+from kojisession import KojiSession
+from rebuild import rebuildPackage, BuildState
+from util import error, whoami
+import configuration
 
 
-async def main():
-    logger = logging.getLogger(__name__)
+async def task_dispatcher(upstream, downstream, packages: list):
+    logger = logging.getLogger(whoami())
 
-    tag = settings.data["tag"]
-    target = settings.data["target"]
-
-    upstream = KojiSession(settings.data.config['upstream'])
-    downstream = KojiSession(settings.data.config['downstream'])
-
-    if tag not in upstream.listTags():
-        error("Tag: %s does not exist in upstream koji database" % tag)
-
-    # create tag and target in downtream instance
-    bootstrap(downstream)
-    
-    # Check for an ignorelist
-    if settings.data.files["ignore"] is not None:
-        try:
-            ignorelist = open(settings.data.files["ignore"])
-        except OSError:
-            raise
-    else:
-        ignorelist = None
-
-    packagelist = open("packages.txt", "w")
-    
-    # Fetch package names from upstream
-    for pkg in upstream.getPackageList(upstream, tag):
-        # Write package names to a file
-        # TODO : Ignore packages from ignorelist
-        packagelist.write(str(pkg) + "\n")
-
-        # Add package to downstream database
-        downstream.packageListAdd(tag = tag, pkg = pkg)
-    
-
-    ignorelist.close()
     task_queue = list()
-    while True:
-        try:
-            pass
-            
-        except KeyboardInterrupt:
-            sys.stdout.write("Received interrupt signal from input. Exiting...\n")
-            break
 
+    while packages or task_queue:
+        ready = downstream.readyHosts(arches[0])
+
+        while len(task_queue) < ready:
+            build_task = asyncio.create_task(rebuildPackage(upstream, downstream, packages.pop(0)))
+            task_queue.append(build_task)
+
+        done, _ = await asyncio.wait(task_queue, return_when=asyncio.FIRST_COMPLETED)
+
+        for task in done:
+            result:dict = await task
+
+            pkg = next(iter(result))
+
+            if result[pkg] == BuildState.FAILED:
+                logger.critical("Package %s build failed!" % pkg)
+
+            task_queue.remove(task)
 
 
 
 if __name__ == "__main__" :
+    try:
+        configfile = os.path.expanduser("/".join([os.getcwd(), "config.yml"]))
+    except FileNotFoundError:
+        try:
+            configfile = sys.argv[1]
+        except IndexError:
+            sys.stderr.write("Configuration file not found! Please provide config file in YAML format!")
+            sys.exit(1)
 
-    settings.data = settings.Data(sys.argv[1])
+    configuration.setup(configfile)
 
-    if settings.data.files['log'] is None:
-        settings.data.files['log'] = os.getcwd() + "/app.log"
-
-    logging.basicConfig(filename = settings.data.files['log'], 
-                        level = logging.INFO, 
+    logging.basicConfig(filename = os.getenv('logfile'),
+                        level = logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s : %(message)s')
 
-    asyncio.run(main)
+    logger = logging.getLogger(__name__)
+
+    upstream = KojiSession(configuration.get_instance('upstream'))
+    downstream = KojiSession(configuration.get_instance('downstream'))
+
+    # XXX : Currently only testing one arch
+    arches = downstream.instance['arches'][0]
+
+    try:
+        dest_tag = downstream.instance['tag']
+    except KeyError:
+        target = downstream.getBuildTarget(downstream.instance['target'])
+        dest_tag = target['dest_tag_name']
+
+    try:
+        upst_tag = upstream.instance['tag']
+    except KeyError:
+        target = upstream.getBuildTarget(upstream.instance['target'])
+        upst_tag = target['dest_tag_name']
+
+    # Check for an ignorelist
+    ignorefile: typing.TextIO | None; ignorelist = list()
+
+    if os.getenv("ignorelist") != 'None':
+        try:
+            ignorefile = open(str(os.getenv("ignorelist")))
+            ignorelist = ignorefile.readlines()
+            ignorefile.close()
+        except (FileNotFoundError, EnvironmentError):
+            ignorefile = ignorelist = None
+            logger.warning("Ignorelist was not specified. Bypassing ignore requests")
+            pass
+
+    # Check for a buildlist
+    buildfile: typing.TextIO ; buildlist: list[str]
+    if os.getenv('buildlist') != 'None':
+        try:
+            buildfile = open(str(os.getenv('buildlist')), "r")
+        except FileNotFoundError:
+            error(f"File {os.getenv('buildlist')} not found!")
+    else:
+        buildfile = open("buildlist.txt", "r+")
+        for pkg in upstream.getPackageList(upst_tag):
+            buildfile.write(pkg + "\n")
+
+    buildlist = buildfile.readlines()  #type: ignore 
+    buildfile.close()       #type: ignore                
+
+    # Remove package names from buildlist that are included in ignorelist
+    if ignorelist:
+        for pkg in ignorelist:
+            if pkg in buildlist:
+                buildlist.remove(pkg)
+
+    # Create list of packages
+    packages = [pkg.strip() for pkg in buildlist]
+
+    # Add packages to downstream koji database
+    for pkg in packages:
+        downstream.packageListAdd(tag = dest_tag, pkg = pkg)
+
+    asyncio.run(task_dispatcher(upstream, downstream, packages))
