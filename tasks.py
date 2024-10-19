df5a9a3968014377dac1f7cec46b74a6aaf2767b
Arif B <arif.dev@pm.me>
2024-10-19 17:40:02 +0530

    Change default polling interval to 5 mins

--- tasks.py
+++ tasks.py
@@ -30,7 +30,7 @@ class TaskWatcher:
         return (state in [TaskState.CLOSED, TaskState.CANCELLED, TaskState.FAILED])
     
 
-async def watch_task(session, task_id, poll_interval: int = 600) -> int:
+async def watch_task(session, task_id, poll_interval: int = 300) -> int:
     """
     :param session - koji client session object
     :param task_id - task id
