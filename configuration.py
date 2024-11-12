86bfaffed7449784e499a18077cf0a9643917408
Arif B <arif.dev@pm.me>
2024-11-12 17:46:26 +0530

    Fix to connect to server when auth is none

--- configuration.py
+++ configuration.py
@@ -5,6 +5,7 @@ from util import resolvepath, error
 from getpass import getpass
 import keyring
 from email_validator import validate_email, EmailNotValidError
+import logging
 
 
 class Configuration:
@@ -12,6 +13,8 @@ class Configuration:
         configfile = os.path.expanduser(configfile)
         with open(configfile, "r") as f:
             self.parameters = yaml.safe_load(f)
+
+        self.logger = logging.getLogger(__name__)
 
     def get_koji_instance(self, name):
         return self.parameters["instance"][name]
@@ -22,13 +25,15 @@ class Configuration:
         except KeyError:
             os.environ["MAIL_NOTIFY"] = "False"
 
-        if os.environ["MAIL_NOTIFY"] == "True":
+        if os.getenv("MAIL_NOTIFY") == "False":
+            return 1
+        else:
             try:
                 os.environ["MAIL_TRIGGER"] = str(self.parameters["email"]["trigger"])
                 os.environ["MAIL_SERVER"] = str(self.parameters["email"]["server"])
                 os.environ["MAIL_USERID"] = str(self.parameters["email"]["sender_id"])
             except:
-                error("Email parameters not set")  # TODO
+                error("Email parameter(s) undefined", exc_info=True)
 
             try:
                 validate_email(str(os.getenv("MAIL_USERID")))
@@ -40,47 +45,48 @@ class Configuration:
             except KeyError:
                 os.environ["MAIL_PORT"] = "587"  # default to 587
 
-            auth = "none"
             try:
-                os.environ["MAIL_AUTH"] = str(self.parameters["email"]["auth"])
+                auth = str(self.parameters["email"]["auth"])
             except KeyError:
-                os.environ["MAIL_AUTH"] = "None"
+                auth = "none"
+            finally:
+                valid_auth = ["none", "tls", "start_tls"]
+                if auth.lower() not in valid_auth:  # type: ignore
+                    self.logger.critical(f'Invalid authentication method "auth"')
+                auth = "none"
+
+            service = "kojibuild"
+            user = str(os.getenv("USER"))
+
+            # Save password in keyring
+            keyring.set_password(
+                service,
+                user,
+                password=getpass(
+                    "Enter password for %s :" % (os.getenv("MAIL_USERID"))
+                ),
+            )
+
+            tls = True if auth == "tls" else False
+            start_tls = True if auth == "start_tls" else False
+
+            client = aiosmtplib.SMTP(
+                hostname=os.getenv("MAIL_SERVER"),
+                port=int(os.getenv("MAIL_PORT")),  # type: ignore
+                username=os.getenv("MAIL_USERID"),
+                password=keyring.get_password(service, user),
+                use_tls=tls,
+                start_tls=start_tls,
+            )
+
+            try:
+                await client.connect()
+            except aiosmtplib.errors.SMTPAuthenticationError:
+                error("ERROR! Could not authenticate to email server")
             else:
-                valid_auth = ["tls", "start_tls"]
-                auth = os.environ["MAIL_AUTH"]
-                if auth.lower() not in valid_auth:
-                    os.environ["MAIL_AUTH"] = "None"
+                self.logger.info("Successfully authenticated to email server")
 
-            if os.getenv("MAIL_AUTH") != "None":
-                service = "kojibuild"
-                user = str(os.getenv("USER"))
-
-                keyring.set_password(
-                    service,
-                    user,
-                    password=getpass(
-                        "Enter password for %s :" % (os.getenv("MAIL_USERID"))
-                    ),
-                )
-
-                tls = True if auth == "tls" else False
-                start_tls = True if auth == "start_tls" else False
-
-                client = aiosmtplib.SMTP(
-                    hostname=os.getenv("MAIL_SERVER"),
-                    port=int(os.getenv("MAIL_PORT")),  # type: ignore
-                    username=os.getenv("MAIL_USERID"),
-                    password=keyring.get_password(service, user),
-                    use_tls=tls,
-                    start_tls=start_tls,
-                )
-
-                try:
-                    await client.connect()
-                except aiosmtplib.errors.SMTPAuthenticationError:
-                    error("ERROR! Could not authenticate to email server")
-                else:
-                    print("Successfully authenticated to email server")
+            return 0
 
     async def setup(self):
         try:
@@ -110,6 +116,6 @@ class Configuration:
                 resolvepath(self.parameters["files"]["logfile"])
             )
         except KeyError:
-            pass
+            pass  # We ignore or set to defaults if not defined
 
         await self.__email_setup()
