96e3d1f8cad85faf79ff1115f793557720ad0eb4
Arif B <arif.dev@pm.me>
2024-11-12 09:39:28 +0530

    Segregate authetication setup and refactored code

--- kojisession.py
+++ kojisession.py
@@ -1,11 +1,9 @@
 import koji
 import os
-import sys
 import time
 import string
 import random
 import logging
-import configparser
 import shutil
 from util import conf_to_dict, nestedseek, error, resolvepath
 
@@ -16,100 +14,94 @@ class KojiSession(koji.ClientSession):
         """Initialize a koji session object
         @param: instance - dictionary object containing koji instance information
         """
+        self.instance = instance
         self.logger = logging.getLogger("kojisession")
 
-        self.instance: dict = instance
-
-        configfile = resolvepath(instance["config"])
-        config = dict()
+        try:
+            configfile = resolvepath(instance["config"])
+            self.config = conf_to_dict(str(configfile))
+        except:
+            error(exc_info=True)
 
         try:
-            config = conf_to_dict(str(configfile))
-        except (FileNotFoundError, PermissionError, configparser.ParsingError) as e:
-            error(msg=e, exc_info=True)
-
-        if not any(config):
-            sys.stderr.write(
-                "Error parsing the configuration file. Check logs for possible issues."
-            )
-            sys.exit(1)
-
-        try:
-            self.server = str(config["server"])
-            self.logger.info("Server set to %s" % self.server)
+            self.server = str(self.config["server"])
         except KeyError:
-            error("Parameter server not defined in %s" % configfile)
+            error("Parameter server not defined in %s" % configfile)  # type: ignore
 
         try:
-            self.auth = str(config["authtype"]).lower()
-        except KeyError as e:
+            self.auth = str(self.config["authtype"]).lower()
+        except KeyError:
             self.auth = None
-            self.logger.warning("Authentication method not defined")
-
-        if self.auth is not None:
-            if self.auth == "ssl":
-                try:
-                    self._ca_cert = os.path.expanduser(config["serverca"])
-                    self._client_cert = os.path.expanduser(config["cert"])
-                    self.set = True
-                except KeyError:
-                    self.logger.warning(
-                        "SSL certificate info not defined in %s" % configfile,
-                        exc_info=True,
-                    )
-                    self.set = False
-                except (FileNotFoundError, PermissionError) as e:
-                    self.logger.warning(e)
-                    self.set = False
-            elif self.auth == "kerberos":
-                try:
-                    self._principal = os.path.expanduser(config["principal"])
-                    self._keytab = os.path.expanduser(config["keytab"])
-                    self.set = True
-                except KeyError:
-                    self.logger.warning(
-                        "Kerberos authentication info not defined in %s" % configfile
-                    )
-                    self.set = False
-                except (FileNotFoundError, PermissionError) as e:
-                    self.logger.warning(e)
-                    self.set = False
-                except TypeError:
-                    pass
-
-        else:
-            self.logger.warning(
-                'Unsupported authentication method "%s" specified in configuration file'
-                % self.auth
-            )
-            self.set = False
 
         # Call parent class constructor
         koji.ClientSession.__init__(self, baseurl=self.server)
 
     """-----------------------------------------------------------------------------------------------------------"""
 
-    def auth_login(self) -> bool:
-        """
-        Login to koji instance using SSL or Keberos authentication
-        """
-        if self.set is not False:
+    def __setup_authentication(self):
+        if self.auth is not None:
             if self.auth == "ssl":
-                info = self.ssl_login(cert=self._client_cert, serverca=self._ca_cert)
+                try:
+                    self._ca_cert = os.path.expanduser(self.config["serverca"])
+                    self._client_cert = os.path.expanduser(self.config["cert"])
+                    self.certs_set = True
+                except KeyError:
+                    self.logger.warning(
+                        "SSL certificate info missing for %s" % self.server,
+                        exc_info=True,
+                    )
+                    self.certs_set = False
+                except (FileNotFoundError, PermissionError) as e:
+                    self.logger.warning(e)
+                    self.certs_set = False
             elif self.auth == "kerberos":
-                info = self.gssapi_login(principal=self._principal, keytab=self._keytab)
+                try:
+                    self._principal = os.path.expanduser(self.config["principal"])
+                    self._keytab = os.path.expanduser(self.config["keytab"])
+                    self.certs_set = True
+                except KeyError:
+                    self.logger.warning(
+                        "Kerberos authentication info missing for %s" % self.server,
+                        exc_info=True,
+                    )
+                    self.certs_set = False
+                except (FileNotFoundError, PermissionError) as e:
+                    self.logger.warning(e)
+                    self.certs_set = False
+                except TypeError:
+                    pass
+
+        else:
+            self.logger.warning(
+                f'Unsupported authentication method "f{self.auth}" specified!'
+            )
+            self.certs_set = False
+
+    def auth_login(self) -> bool:
+        """Login to koji instance using SSL or Keberos authentication"""
+        self.__setup_authentication()
+
+        if self.certs_set:
+            if self.auth == "ssl":
+                response = self.ssl_login(
+                    cert=self._client_cert, serverca=self._ca_cert
+                )
+            elif self.auth == "kerberos":
+                response = self.gssapi_login(
+                    principal=self._principal, keytab=self._keytab
+                )
             else:
                 self.logger.critical(
                     'Unsupported authentication method "%s" specified' % self.auth
                 )
                 return False
 
-            if info is True:
+            if response is True:
                 self.logger.info(
                     "Logged in as %s@%s. Authenticated via %s"
                     % ((self.getLoggedInUser()).get("name"), self.server, self.auth)
                 )
-            return info
+            return response
         else:
             return False
 
@@ -134,7 +126,7 @@ class KojiSession(koji.ClientSession):
 
     """-----------------------------------------------------------------------------------------------------------"""
 
-    def isNoArch(self, tag, pkg):
+    def is_pkg_noarch(self, tag, pkg):
         noarch = ["src", "noarch"]
         builds = list()
 
@@ -156,36 +148,41 @@ class KojiSession(koji.ClientSession):
 
     """-----------------------------------------------------------------------------------------------------------"""
 
-    def getPackageList(self, tag):
+    def get_package_list(self, tag):
         res = self.listPackages(tagID=tag)
         if res is not None:
             packages = nestedseek(res, "package_name")
             for pkg in packages:
                 yield pkg
         else:
-            self.logger.info(f"No package tagged under tag :{tag}")
+            self.logger.info(f"No package tagged under tag : {tag}")
             return None
 
     """-----------------------------------------------------------------------------------------------------------"""
 
     def importPackage(self, pkgdir, tag, prune_dir: bool = True):
+        """Download and import package to koji instance
+        :param pkgdir: str - Path to directory where packages are downloaded
+        :param tag: str - Tag name under which package will be tagged
+        :param prune_dir: bool - If pkgdir should be deleted to save disk space
+        """
 
         def unique_path(prefix):
             """Create a unique path fragment by appending a path component to prefix."""
             return "%s/%r.%s" % (
                 prefix,
                 time.time(),
-                "".join([random.choice(string.ascii_letters) for i in range(8)]),
+                "".join([random.choice(string.ascii_letters) for _ in range(8)]),
             )
 
         if not os.path.exists(pkgdir):
             self.logger.critical(f"Directory {pkgdir} does not exist")
-            return False
+            return 1
 
         if self.getSessionInfo() is None:
             if self.auth_login() == False:
                 self.logger.critical("You must be logged in to import packages")
-                return False
+                return 1
 
         for rpm in list(os.listdir(pkgdir)):
             localfile = "/".join([pkgdir, rpm])
@@ -221,12 +218,14 @@ class KojiSession(koji.ClientSession):
 
     """-----------------------------------------------------------------------------------------------------------"""
 
-    def totalHosts(self, arch):
+    def get_total_hosts(self, arch):
+        """Get total number of hosts available for a specified architecture"""
         return len(self.listHosts(arches=[arch], enabled=True, channelID="default"))
 
     """-----------------------------------------------------------------------------------------------------------"""
 
-    def readyHosts(self, arch):
+    def get_ready_hosts(self, arch):
+        """Get number of hosts for specified architecture that are ready to take build job"""
         return len(
             self.listHosts(arches=[arch], enabled=True, ready=True, channelID="default")
         )
