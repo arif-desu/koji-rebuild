0b5ff74807d5735d7a9cbb4cf6a402d1777ca22b
Arif B <arif.dev@pm.me>
2024-11-25 14:52:30 +0530

    Change max jobs queued according to environment variable

--- dispatcher.py
+++ dispatcher.py
@@ -2,22 +2,16 @@ import asyncio
 import logging
 from notification import Notification
 from rebuild import Rebuild, BuildState
-from util import whoami, error
+from util import whoami
+import os
 
 
 async def task_dispatcher(
     upstream, downstream, packages: list, notify: Notification | None = None
 ):
     logger = logging.getLogger(whoami())
-
     task_queue = list()
-
-    # XXX: Currently checking only one arch
-    arch = (downstream.instance["arches"])[0]
-
-    if downstream.get_total_hosts(arch) == 0:
-        error(f"No builders available for architecture {arch}")
-
+    max_jobs = int(os.getenv("MAX_JOBS", default=10))  # type: ignore
     rebuild = Rebuild(upstream, downstream)
 
     def get_taskurl(session, task_id: int):
@@ -27,9 +21,7 @@ async def task_dispatcher(
         return url
 
     while packages or task_queue:
-        ready = downstream.get_ready_hosts(arch)
-
-        while len(task_queue) < ready and packages:
+        while len(task_queue) < max_jobs and packages:
             build_task = asyncio.create_task(rebuild.rebuild_package(packages.pop(0)))
             task_queue.append(build_task)
 
